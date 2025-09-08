f9045bc0273b00e65e735bd94d5c05833a80d492
Priyanka_Mandal <Priyanka_Mandal@outlook.com>
2025-09-08 13:44:48 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import signupImg from "../../../assets/Images/signup.png"
+import signupImg from "../../../assets/images/signup.png"
 import Template from "../components/core/Auth/Template"
 
 function Signup() {
