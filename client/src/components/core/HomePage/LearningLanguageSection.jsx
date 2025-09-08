63ecb173c6c4c1d1830f76ea1d84b33496740816
Priyanka_Mandal <Priyanka_Mandal@outlook.com>
2025-09-08 14:37:23 +0530

    Update LearningLanguageSection.jsx

--- client/src/components/core/HomePage/LearningLanguageSection.jsx
+++ client/src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import HighlightText from './HighlightText'
-import know_your_progress from "../../../../../assets/Images/Know_your_progress.png"
-import compare_with_others from "../../../../../assets/Images/Compare_with_others.png"
-import plan_your_lesson from "../../../../../assets/Images/Plan_your_lessons.png"
+import know_your_progress from "../../../../../assets/images/Know_your_progress.png"
+import compare_with_others from "../../../../../assets/images/Compare_with_others.png"
+import plan_your_lesson from "../../../../../assets/images/Plan_your_lessons.png"
 import CTAButton from "../HomePage/Button"
 
 const LearningLanguageSection = () => {
