826476149bd5565f747927e08370e2286ee56d15
Priyanka_Mandal <Priyanka_Mandal@outlook.com>
2025-09-08 14:32:16 +0530

    Update CourseDetails.jsx

--- client/src/pages/CourseDetails.jsx
+++ client/src/pages/CourseDetails.jsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 
 
-import ConfirmationModal from "../components/Common/ConfirmationModal"
+import ConfirmationModal from "../components/common/ConfirmationModal"
 import Footer from "../components/common/Footer"
 import RatingStars from "../components/Common/RatingStars"
 import CourseAccordionBar from "../components/core/Course/CourseAccordionBar"
